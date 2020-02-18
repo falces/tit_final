8501e21875bcb055c1b19b28fa11292382bf76d7
Javi <falces@gmail.com>
2020-02-18 22:04:02 +0100

    Servicios

---
+++ src/Services/Budgets.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Budgets;
+
+/**
+ * FRONTOFFICE TOOL
+ * BACKOFFICE MANAGER
+ * LIST
+ * STATUS
+ */
+class Budgets
+{
+	/**
+	 * Descripción de la función
+	 */
+	public
+	function nombreFuncion()
+	{
+		//
+	}
+}
